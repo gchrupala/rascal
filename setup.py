3b4f1a9490ed72194d5852eb0a16a6a869b02dda
Grzegorz Chrupała <g.chrupala@uvt.nl>
2019-06-21 16:50:59 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+from setuptools import setup
+
+setup(name='rascal',
+      version='0.4',
+      description='Visually grounded speech with inductive biases',
+      url='https://github.com/gchrupala/vgs',
+      author='Grzegorz Chrupała',
+      author_email='g.chrupala@uvt.nl',
+      license='MIT',
+      packages=['onion','vg', 'vg.defn'],
+      zip_safe=False,
+      install_requires=[
+          'torch == 1.0.0',
+          'torchvision == 0.2.1',
+          'scikit-learn == 0.20.1',
+          'scipy == 1.2.0rc2',
+          'python-Levenshtein == 0.12.0'
+          
+      ])
